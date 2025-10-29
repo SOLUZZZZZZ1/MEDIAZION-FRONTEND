aaf9afe46f3f28d9b31ec75f2a115146cfd819ad
mediazion <soluzziona@gmail.com>
2025-10-29 10:31:32 +0100

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,3 +1,4 @@
+// src/App.jsx
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
@@ -14,15 +15,15 @@ import Actualidad from "./pages/Actualidad.jsx";
 import MediadorAlta from "./pages/MediadorAlta.jsx";
 import Success from "./pages/Success.jsx";
 import Cancel from "./pages/Cancel.jsx";
+
 import AdminLogin from "./pages/admin/Login.jsx";
 import AdminDashboard from "./pages/admin/Dashboard.jsx";
+
 import MediadoresDirectorio from "./pages/MediadoresDirectorio.jsx";
 import PanelMediador from "./pages/PanelMediador.jsx";
+
 import CourseDetail from "./pages/CourseDetail.jsx";
 import WebinarDetail from "./pages/WebinarDetail.jsx";
-import AvisoLegal from "./pages/AvisoLegal.jsx";
-import Rgpd from "./pages/Rgpd.jsx";
-import Cookies from "./pages/Cookies.jsx";
 
 export default function App() {
   return (
@@ -38,28 +39,31 @@ export default function App() {
     >
       <Navbar />
       <Routes>
+        {/* Básicas */}
         <Route path="/" element={<Inicio />} />
         <Route path="/quienes-somos" element={<QuienesSomos />} />
         <Route path="/servicios" element={<Servicios />} />
-        <Route path="/servicios/curso/:slug" element={<Success />} /> {/* si usas CourseDetail, cambia aquí */}
-        <Route path="/servicios/webinar/:slug" element={<Cancel />} /> {/* si usas WebinarDetail, cambia aquí */}
+        <Route path="/tarifas" element={<Tarifas />} />
+        <Route path="/contacto" element={<Contacto />} />
+        <Route path="/actualidad" element={<Actualidad />} />
+
+        {/* Mediadores */}
         <Route path="/mediadores" element={<Mediadores />} />
         <Route path="/mediadores/directorio" element={<MediadoresDirectorio />} />
         <Route path="/mediadores/alta" element={<MediadorAlta />} />
         <Route path="/panel-mediador" element={<PanelMediador />} />
-        <Route path="/tarifas" element={<Tarifas />} />
-        <Route path="/contacto" element={<Contacto />} />
-        <Route path="/actualidad" element={<Actualidad />} />
-        <Route path="/suscripcion/ok" element={<Success />} />
-        <Route path="/suscripcion/cancel" element={<Cancel />} />
-        <Route path="/admin" element={<AdminLogin />} />
-        <Route path="/admin/panel" element={<AdminDashboard />} />
-        {/* rutas del footer */}
-        <Route path="/aviso-legal" element={<AvisoLegal />} />
-        <Route path="/rgpd" element={<Rgpd />} />
-        <Route path="/cookies" element={<Cookies />} />
+
+        {/* Detalle formativo */}
         <Route path="/servicios/curso/:slug" element={<CourseDetail />} />
         <Route path="/servicios/webinar/:slug" element={<WebinarDetail />} />
+
+        {/* Suscripción */}
+        <Route path="/suscripcion/ok" element={<Success />} />
+        <Route path="/suscripcion/cancel" element={<Cancel />} />
+
+        {/* Admin */}
+        <Route path="/admin" element={<AdminLogin />} />
+        <Route path="/admin/panel" element={<AdminDashboard />} />
       </Routes>
       <Footer />
     </div>
